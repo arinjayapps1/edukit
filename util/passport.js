a96cd8389e253f4dfe71ce4821cb0fcb57c00158
arinjayapps1 <68187083+arinjayapps1@users.noreply.github.com>
2023-03-04 13:14:31 +0530

    changed passport

--- util/passport.js
+++ util/passport.js
@@ -29,7 +29,7 @@ passport.serializeUser((user, cb) => {
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://orca-app-cpslt.ondigitalocean.app/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   function(accessToken, refreshToken, profile, cb) {
    console.log(profile);
